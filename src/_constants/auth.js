2d0791125c71e755978ff4447cac2613396cfd33
Váci Kolléga <vacikorni@freemail.hu>
2018-11-06 19:33:52 +0100

    Firebase added / npm install kelleni fog

---
+++ src/_constants/auth.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBvVQfwaHKADurzq1nAQpwe8XWtZPIDK5M",
+    authDomain: "oop3-b2dc9.firebaseapp.com",
+    databaseURL: "https://oop3-b2dc9.firebaseio.com",
+    projectId: "oop3-b2dc9",
+    storageBucket: "oop3-b2dc9.appspot.com",
+    messagingSenderId: "860724138032"
+};
+
+firebase.initializeApp(config);

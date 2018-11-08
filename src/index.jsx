62207ec1d70e12fa6cdb8f760ffa701f005cf835
Arpad Kovac <karpad2@gmail.com>
2018-11-08 13:17:50 +0100

    Javitva

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux';
 import { store } from './_helpers';
 import { App } from './App';
 // setup fake backend
-import { configureFakeBackend } from './_helpers';
-configureFakeBackend();
+import { configureBackend } from './_helpers';
+configureBackend();
 
 render(
     <Provider store={store}>

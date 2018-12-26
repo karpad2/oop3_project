9cecbf362fee5e44cb58972e492fa4d6c27103b2
Váci Kolléga <vacikorni@freemail.hu>
2018-12-26 14:55:43 +0100

    Badge from database

--- students_list.php
+++ students_list.php
@@ -12,8 +12,7 @@ include "db_config.php";
 $sql="SET NAMES utf8";
 mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
-$sql="SELECT students.Student_id, students.student_name,  IFNULL(SUM(Point),0) as 'Point' FROM students left join students_points on students.Student_id = students_points.Student_id
-GROUP BY students.student_id order by Point DESC";
+$sql="SELECT * FROM list_student_by_points";
 
 //$sql="SELECT Student_id FROM students ";
 $result=mysqli_query($conn,$sql) or die(mysqli_error($conn));
@@ -59,23 +58,23 @@ echo '
     <table id="studenttable" class="lTable">
       <tbody>';
         //$i=0;
-        $max=$users[0]["Point"];
+        $max=$users[0]["points"];
         //foreach ($users as $user) {
         for ($i = 1; $i <= 5; $i++) {
           //++$i;
-          $current=$users[$i-1]["Point"];
+          $current=$users[$i-1]["points"];
           $user = $users[$i-1];
           $percent = floor($current / $max * 100);
           $current=number_format($current, 0, " ", " ");
           if ($percent == 0) $percent++;
 
-          $rand = rand(0,12);
+          $rand = $user["cnt"];
 
           echo "
           <tr>
             <th scope=\"row\"><span class=\"number\" title=\"Ez nem id, hanem sorszám.\">$i.</span></th>
             <td>
-              <div class=\"student_id\">{$user["Student_id"]}</div>
+              <div class=\"student_id\">{$user["student_id"]}</div>
               <div class=\"progress_container\"><div class=\"progressBar\" style=\"width:{$percent}%\"></div></div>
             </td>
             <td><div class=\"student_points\">{$current}<br>points</div></td>
@@ -105,19 +104,19 @@ echo '
           <tbody id="hidden" class="hidden_table">';
           for ($i = 6; $i < count($users); $i++) {
             //++$i;
-            $current=$users[$i-1]["Point"];
+            $current=$users[$i-1]["points"];
             $user = $users[$i-1];
             $percent = floor($current / $max * 100);
             $current=number_format($current, 0, " ", " ");
             if ($percent == 0) $percent++;
 
-            $rand = rand(0,12);
+            $rand = $user["cnt"];
 
             echo "
             <tr>
               <th scope=\"row\"><div class=\"number\">$i.</div></th>
               <td>
-                <div class=\"student_id\">{$user["Student_id"]}</div>
+                <div class=\"student_id\">{$user["student_id"]}</div>
                 <div class=\"progress_container\"><div style=\"width:{$percent}%\" class=\"progressBar\"></div></div>
               </td>
               <td><div class=\"student_points\">{$current}<br>points</div></td>
